5a9cf75b3bddcc921357173c007fe6e9bf305357
Abdullah-Rehman786 <Air711@live.com>
2022-03-12 07:32:32 -0600

    Completed exercises for Ch4

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+
+public class Course {
+    private ArrayList<Student> students;
+    private String instructorName;
+    private String semesterOffered;
+}
